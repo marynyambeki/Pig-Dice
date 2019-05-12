3e256c1518e48c644610a647fef8d2bff753c506
mary <marynyambeki1@gmail.com>
2019-05-12 15:25:38 +0300

    page layout changes when there is a current player

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
 // jshint esversion: 6
-const p1Score = document.querySelector('.p1Score');
+const p1Score = document.querySelector('.player1Score');
+const p2Score = document.querySelector('.player2Score');
+const player1 = document.querySelector('.player1Header button');
+const player2 = document.querySelector('.player2Header button');
+const p1TempScore = document.querySelector('.player1TotalScoreNo');
+const p2TempScore = document.querySelector('.player2TotalScoreNo');
+const newGameBtn = document.querySelector('.newGameBtn');
+const keepBtn = document.querySelector('.keepBtn button');
+const diceImg = document.querySelector('.diceJS');
+const rollBtn = document.querySelector('.rollBtn button');
+
+let currentPlayer;
+
 
   
