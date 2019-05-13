6fa25c9f50ec68c7e2d100809adf04b62903dd0c
mary <marynyambeki1@gmail.com>
2019-05-13 12:03:18 +0300

    Added player functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 var player1="";
 var player2="";
 
+var throwdice = function () {
+    return Math.floor(6 * Math.random())+1;
+}
+function Player(turn) {
+    this.roll = 0;
+    this.rollscore = 0;
+    this.totalscore = 0;
+    this.turn = turn;
+    this.players;
+}
