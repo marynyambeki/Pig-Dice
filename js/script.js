60aee50140472937607af81890d890878c29399e
mary <marynyambeki1@gmail.com>
2019-05-16 08:36:25 +0300

    given random function of roll of numbers

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ var diceTotal2 = [];
 var total1, total2;
 var final1 = 0;
 var final2 = 0;
+
+function roll() {
+    random = Math.floor(Math.random() * 6 + 1);
+}
+
+//Getting sum in an array
+function getSum(total, dice) {
+    return total + dice;
+}
